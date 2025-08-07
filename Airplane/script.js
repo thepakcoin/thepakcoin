9d9d01b682c816021bc9ef2bab1d064a261af743
The Pak Coin <thepakcoin@gmail.com>
2025-08-07 20:49:14 +0500

    Update script.js

--- Airplane/script.js
+++ Airplane/script.js
@@ -1,1 +1,19 @@
+const player = document.getElementById("player");
+let playerPos = window.innerWidth / 2;
 
+document.addEventListener("keydown", movePlayer);
+
+function movePlayer(e) {
+  if (e.key === "ArrowLeft") {
+    playerPos -= 20;
+  }
+  if (e.key === "ArrowRight") {
+    playerPos += 20;
+  }
+
+  // Board ke andar limit
+  if (playerPos < 0) playerPos = 0;
+  if (playerPos > window.innerWidth - 40) playerPos = window.innerWidth - 40;
+
+  player.style.left = `${playerPos}px`;
+}
