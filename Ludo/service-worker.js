4ef940ae003b01580ddf512cd6aed955f2a07161
The Pak Coin <thepakcoin@gmail.com>
2025-08-09 00:07:01 +0500

    Create service-worker.js

---
+++ Ludo/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("ludo-cache").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "script.js",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
