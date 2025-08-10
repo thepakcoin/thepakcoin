f35d227bb1ce60daa66f1e194d9d1d25d3ff7e67
The Pak Coin <thepakcoin@gmail.com>
2025-08-10 07:41:55 +0500

    Delete Ludo/script.js

--- Ludo/script.js
+++
@@ -1,8 +0,0 @@
-const board = document.querySelector('.ludo-board');
-
-// Create 15x15 = 225 cells
-for (let i = 0; i < 225; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  board.appendChild(cell);
-}
