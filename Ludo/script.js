543f8e98650effb5d75be235d5857e253ca6f880
The Pak Coin <thepakcoin@gmail.com>
2025-08-10 10:19:38 +0600

    Delete Ludo/script.js

--- Ludo/script.js
+++
@@ -1,9 +0,0 @@
-const cells = document.querySelectorAll('.cell');
-
-// Har cell pe click hone par position print hoga
-cells.forEach(cell => {
-    cell.addEventListener('click', () => {
-        // Cell ki position console mein print ho jayegi
-        console.log(`Cell clicked at position: ${cell}`);
-    });
-});
